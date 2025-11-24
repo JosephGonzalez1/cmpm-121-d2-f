043ecd40f07b4a0765c6dc0dd98db614c65d8f0f
JosephGonzalez1 <152119091+JosephGonzalez1@users.noreply.github.com>
2025-11-24 19:37:39 +0000

    Added a thin and thick marker to use on the canvas with one class. (step 6 complete)

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,35 @@ const title = document.createElement("h1");
 title.textContent = "My Drawing App";
 document.body.append(title);
 
+const thinButton = document.createElement("button");
+thinButton.textContent = "Thin";
+document.body.append(thinButton);
+
+const thickButton = document.createElement("button");
+thickButton.textContent = "Thick";
+document.body.append(thickButton);
+
+let currentThickness = 2;
+
+const updateToolButtons = () => {
+  thinButton.classList.remove("selectedTool");
+  thickButton.classList.remove("selectedTool");
+  if (currentThickness === 2) thinButton.classList.add("selectedTool");
+  else thickButton.classList.add("selectedTool");
+};
+
+thinButton.addEventListener("click", () => {
+  currentThickness = 2;
+  updateToolButtons();
+});
+
+thickButton.addEventListener("click", () => {
+  currentThickness = 8;
+  updateToolButtons();
+});
+
+updateToolButtons();
+
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
@@ -28,9 +57,11 @@ type Point = { x: number; y: number };
 
 class MarkerLine {
   points: Point[];
+  thickness: number;
 
-  constructor(x: number, y: number) {
+  constructor(x: number, y: number, thickness: number) {
     this.points = [{ x, y }];
+    this.thickness = thickness;
   }
 
   drag(x: number, y: number) {
@@ -39,6 +70,7 @@ class MarkerLine {
 
   display(ctx: CanvasRenderingContext2D) {
     if (this.points.length < 2) return;
+    ctx.lineWidth = this.thickness;
     ctx.moveTo(this.points[0].x, this.points[0].y);
     for (let i = 1; i < this.points.length; i++) {
       ctx.lineTo(this.points[i].x, this.points[i].y);
@@ -52,7 +84,7 @@ let currentCommand: MarkerLine | null = null;
 const redoStack: MarkerLine[] = [];
 
 canvas.addEventListener("mousedown", (e) => {
-  currentCommand = new MarkerLine(e.offsetX, e.offsetY);
+  currentCommand = new MarkerLine(e.offsetX, e.offsetY, currentThickness);
   displayList.push(currentCommand);
   redoStack.length = 0;
   canvas.dispatchEvent(new Event("drawing-changed"));
@@ -94,9 +126,10 @@ redoButton.addEventListener("click", () => {
 
 canvas.addEventListener("drawing-changed", () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
+
   for (const command of displayList) {
+    ctx.beginPath();
     command.display(ctx);
+    ctx.stroke();
   }
-  ctx.stroke();
 });
